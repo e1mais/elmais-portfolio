8e11f66646e541d3948d512a612b22d5205a322a
e1mais <ismael.martinez161@std.uni.edu.ni>
2023-01-02 17:50:39 -0600

    set code style changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core"
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
